3ed9d3baf8aa26e9cefa88d746ef381312cf1bfe
Robert Chen <bergintonatcapital@gmail.com>
2022-10-08 23:03:37 -0400

    Hover affect over squares to change color

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container');
+let square = document.querySelectorAll('.square');
 
-
+// Sets up css container's Grid to have the input # of rows and columns
 function setGridXX(row, column) {
     container.style.cssText = `grid-template-columns: repeat(${column}, 1fr); 
     grid-template-rows: repeat(${row}, 1fr);`;
 }
 
-function createDiv(numSquares) {
+function createSquares(numSquares) {
     for(let i = 0; i < numSquares; i++) {
         let div = document.createElement('div');
         div.classList.add("square");
@@ -19,8 +20,14 @@ function startSketch() {
     let column = 16;
     let numSquares = row * column;
 
-    createDiv(numSquares);
+    createSquares(numSquares);
     setGridXX(row, column);
+
+    // Listens to user's mouse hover over individual squares and change the background color
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', function (e) {
+        square.style.backgroundColor = "black";
+    }));
 }
 
 startSketch();
